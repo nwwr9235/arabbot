fbb84bb705fa66d98a62334cd8f75bb4e65682fb
nwwr9235 <nwwr9235@gmail.com>
2026-03-19 02:14:16 +0300

    Update __init__.py

--- bot/plugins/py_tgcalls/__init__.py
+++ bot/plugins/py_tgcalls/__init__.py
@@ -1,1 +1,9 @@
+class PyTgCalls:
+def init(self, client):
+self.client = client
 
+async def start(self):
+    print("PyTgCalls stub started (voice disabled)")
+
+async def stop(self):
+    print("PyTgCalls stub stopped")
