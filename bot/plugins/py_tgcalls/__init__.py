a1874cea88461b005801bd73aeb78b00a215a619
nwwr9235 <nwwr9235@gmail.com>
2026-03-19 03:28:00 +0300

    Delete bot/plugins/py_tgcalls directory

--- bot/plugins/py_tgcalls/__init__.py
+++
@@ -1,9 +0,0 @@
-class PyTgCalls:
-def init(self, client):
-self.client = client
-
-async def start(self):
-    print("PyTgCalls stub started (voice disabled)")
-
-async def stop(self):
-    print("PyTgCalls stub stopped")
